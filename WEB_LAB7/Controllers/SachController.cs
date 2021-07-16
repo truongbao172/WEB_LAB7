9168a296ed939b9057341b09376b28761ee0d746
TTMT Hutech <TTMT Hutech@DESKTOP-K2V03PI>
2021-07-16 17:58:59 +0700

    Add project files.

---
+++ WEB_LAB7/Controllers/SachController.cs
@@ -0,0 +1,42 @@
+﻿using WEB_LAB7.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WEB_LAB7.Controllers
+{
+    public class SachController : ApiController
+    {
+        Sach[] sachs = new Sach[]
+         {
+          new Sach { Id = 1, Title = "Tôi Thấy Hoa đỏ Trên Cỏ Non", AuthorName ="Nguyễn Nhật Ánh", Price = 1, Content="Truyện kể về Tuổi thơ..." },
+            new Sach { Id = 2, Title = "Pro ASP.NET MVC5", AuthorName = "Adam Freeman", Content="The ASP.NET MVC 5 Framework is the latest evolution of Microsoft’sASP.NET web platform.", Price = 3.75M },
+
+         };
+
+        public IEnumerable<Sach> GetAll()
+        {
+            return sachs;
+        }
+        public IHttpActionResult GetSach(int id)
+        {
+            var sach = sachs.FirstOrDefault((p) => p.Id == id);
+            if (sach == null)
+            {
+                return NotFound();
+            }
+            return Ok(sach);
+        }
+
+       
+
+    }
+   
+}
+   
+   
+
+
---
+++ WEB_LAB7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WEB_LAB7.WebApiApplication" Language="C#" %>
